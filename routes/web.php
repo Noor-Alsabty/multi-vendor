35ab7aeae913a282d37ef23dd04902ee866475c5
Noor-Asabty <nooralsabty76@gmail.com>
2026-02-12 18:38:42 +0300

    Create vendor dashboard layout with products, orders, financial sections

--- routes/web.php
+++ routes/web.php
@@ -3,24 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use  App\Http\Controllers\StoreController;
 use  App\Http\Controllers\dashboardController;
+use App\Http\Controllers\VendorController;
 
-
-Route::get('/', function () {
-    return view('dashboard');
-});
-// 
-
-Route::get('/d', [dashboardController::class, 'index'])->name('dashboard');
-
-
-
-// 
-
-Route::get('/store',[StoreController::class,'index'])->name('store.index');
-
-Route::get('/create',[StoreController::class,'create'])->name('store.create');
-Route::post('/store',[StoreController::class,'store'])->name('store.store');
-Route::get('/store/{store}/edit',[StoreController::class,'edit'])->name('store.edit');
-Route::put('/store/{store}',[StoreController::class,'update'])->name('store.update');
-Route::get('/show/{store}',[StoreController::class,'show'])->name('store.show');
-Route::delete('/delete/{store}',[StoreController::class,'destroy'])->name('store.destroy');
+Route::get('/vendor/dashboard',[VendorController::class,'index'])->name('vendor.dashboard');
