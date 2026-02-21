9ee3d0ade95daa6b578e5bdbbaeb94390067098b
Noor-Asabty <nooralsabty76@gmail.com>
2026-02-22 02:09:46 +0300

    refactor: move is_active column to a separate migration file

--- database/migrations/2026_02_10_144012_create_products_table.php
+++ database/migrations/2026_02_10_144012_create_products_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
   $table->string('name');
   $table->text('description')->nullable();
   $table->decimal('price',10,2);
-  $table->boolean('is_active')->default(true);
   $table->integer('views')->default(0);
             $table->timestamps();
         });
