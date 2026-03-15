be75c935aaf002bcccb5798aa5b4f4d6ea6c707a
Noor-Asabty <nooralsabty76@gmail.com>
2026-03-15 21:56:01 +0300

    local merge and fix conflicts

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,6 +9,7 @@ use App\Models\ProductVariant;
 use App\Models\Notification;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 class ProductController extends Controller
