df3d99fcc65315938985e620132f8e229c3776c6
Noor-Asabty <nooralsabty76@gmail.com>
2026-04-17 17:56:19 +0300

    adding page to enable vendor see his wallet

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Wallet;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
 
 class WalletController extends Controller
@@ -13,7 +14,46 @@ class WalletController extends Controller
      */
     public function index()
     {
-        //
+        $user = Auth::user();
+        $vendor = $user?->vendor;
+
+        if (!$vendor) {
+            return view('vendor.wallet.index', [
+                'wallet' => null,
+                'totalDeposits' => 0,
+                'totalWithdrawals' => 0,
+                'transactions' => collect(),
+            ]);
+        }
+
+        $wallet = $vendor->wallet()->firstOrCreate([], ['balance' => 0]);
+        $transactions = $wallet->transactions()->latest()->get();
+
+        // Build vendor-local order sequence for deposit transactions only.
+        $depositIdsByOrder = $wallet->transactions()
+            ->where('type', 'deposit')
+            ->oldest()
+            ->pluck('id')
+            ->values();
+
+        $depositSequenceById = $depositIdsByOrder
+            ->flip()
+            ->map(fn ($index) => $index + 1);
+
+        $transactions->transform(function ($transaction) use ($depositSequenceById) {
+            $transaction->display_description = $transaction->description;
+
+            if ($transaction->type === 'deposit' && isset($depositSequenceById[$transaction->id])) {
+                $transaction->display_description = 'أرباح الطلب رقم: ' . $depositSequenceById[$transaction->id];
+            }
+
+            return $transaction;
+        });
+
+        $totalDeposits = $wallet->transactions()->deposits()->sum('amount');
+        $totalWithdrawals = $wallet->transactions()->withdrawals()->sum('amount');
+
+        return view('vendor.wallet.index', compact('wallet', 'totalDeposits', 'totalWithdrawals', 'transactions'));
     }
 
     /**
